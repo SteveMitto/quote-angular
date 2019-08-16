053b99a35376c0b9526124a6f7f103b4eca49015
Steve <mittosteve@gmail.com>
2019-08-16 16:30:43 +0300

    This commit displays description of quote

--- src/app/quote-b.ts
+++ src/app/quote-b.ts
@@ -1,6 +1,6 @@
 export class QuoteB {
     showQuote:boolean;
-  constructor(public id:number,public username:string,public quoteDesc: string,public postenOn:Date,public upVote:number ){
+  constructor(public id:number,public username:string,public description: string,public postenOn:Date,public upVote:number ){
     this.showQuote = false;
   }
 }
